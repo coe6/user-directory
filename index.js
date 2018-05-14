d5ca91af14b7479a60eef3b619245b8a6663e997
Sam Coe <coe6@purdue.edu>
2018-05-14 14:14:21 -0400

    Complete HW assignments

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('It works!')
+//console.log('It works!')
+
+function changeText(ev) {
+    if(ev.target.textContent === 'Change Header!') {
+        const paragraph = document.querySelector('.header1')
+        paragraph.textContent = "USER DIRECTORY"
+    } else if(ev.target.textContent === 'Change Second Header!') {
+        const paragraph = document.querySelector('.header2')
+        paragraph.textContent = "SECOND HEADER"
+    } else if(ev.target.textContent === 'Change Third Header!') {
+        const paragraph = document.querySelector('.header3')
+        paragraph.textContent = document.getElementById("changeThirdHeader").value
+        document.getElementById("changeThirdHeader").value = ' '
+    }
+}
+
+const button = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
+const button3 = document.querySelector('#button3')
+
+button.addEventListener('click', changeText)
+button2.addEventListener('click', changeText)
+button3.addEventListener('click', changeText)
